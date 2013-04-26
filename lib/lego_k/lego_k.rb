f766d2a91280b065054f5a57cc9a273a9de42129
Alex Sinishin <sinishin@gmail.com>
2013-04-26 15:53:43 -0700

    Removed commented lines

--- lib/lego_k/lego_k.rb
+++ lib/lego_k/lego_k.rb
@@ -33,7 +33,6 @@ module LegoK
 
     # Low level methods of operating with listings within a page
     def self.detect_new_listings(page, last_loaded_listing_id) # returns ids of appropirated listings or []
-      #ids = page.search("input[name='ilIds']").attr('value').value.split(',')
       ids = page.search("input[name='ilIds']").first.attr('value').split(',')
       ids.inject([]) do |r, e|
         if e > last_loaded_listing_id
